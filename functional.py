e50500064c26638483ce39571f182a2d47b73e55
jhnns-br <67393696+jhnns-br@users.noreply.github.com>
2020-06-25 00:20:00 +0200

    Add files via upload

---
+++ functional.py
@@ -0,0 +1,93 @@
+"""
+Functions for training of and inference 
+on the causal functional graph
+"""
+
+import numpy as np 
+import pandas as pd 
+import torch as torch
+from torch import nn
+from sklearn import metrics
+import copy
+
+def train_graph(modeled_graph, max_epochs, criterion):
+    """
+    Train the models of the graph node using the 
+    provided dataloaders and optimizers. 
+    """
+    trained_graph = copy.deepcopy(modeled_graph)
+    print('\nTRAINING:')
+    for node in trained_graph:
+        if node['inputs'] is not None:
+            print('\n------------------\t', node['name'], '\t------------------')
+            for epoch in range(max_epochs):
+                """ 
+                TBD: add early stopping support 
+                """
+                train_loss = 0.0
+                stat_train_loss = 0.0 
+                test_loss = 0.0
+                stat_test_loss = 0.0
+
+                node['model'].train()
+                for _, (features, labels) in enumerate(node['trainloader']):
+                    node['optimizer'].zero_grad()
+                    out = node['model'](features)    
+                    train_loss = criterion(out, labels)
+                    train_loss.backward()
+                    node['optimizer'].step()
+                    stat_train_loss += train_loss.item()
+
+                node['model'].eval()
+                for _, (features, labels) in enumerate(node['valloader']):     
+                    out = node['model'](features)  
+                    test_loss = criterion(out, labels)
+                    stat_test_loss += test_loss.item()
+  
+                print('Ep', epoch, '\tTrain Loss:',
+                    round(stat_train_loss/len(node['trainloader']),5), '\tVal. Loss:',
+                    round(stat_test_loss/len(node['valloader']), 5))
+
+    return trained_graph
+
+def inference(trained_graph, df_inputs, df_labels=None, metric=['MAE']):
+    """
+        Function uses input values provided in df_inputs.
+        If df_inputs containes values for node_x
+        the values in df_inputs will be used as inputs for 
+        subsequent node_x+1, not the predictions of node_x. 
+
+        TBD: graph must be strucutred sequentially when created 
+            --> implent so some sort of sorting algo
+    """
+    
+    df_pred = pd.DataFrame()
+    for node in trained_graph:
+        if node['inputs'] is not None:
+            X = torch.tensor(df_inputs[node['inputs']].to_numpy(), dtype=torch.double)
+            y = []
+            for x in X:
+                y_pred = node['model'](x)
+                y.append(y_pred.item())
+            df_pred[node['name']] = y
+            df_inputs[node['name']] = y
+    
+    if df_labels is not None:
+        print('\nTESTING:\n')
+        for label in df_labels.columns:
+            for m in metric:
+                if m == 'MAE':
+                    res = metrics.mean_absolute_error(df_labels[[label]].to_numpy(), 
+                                                    df_pred[[label]].to_numpy())
+                elif m == 'MSE':
+                    res = metrics.mean_squared_error(df_labels[[label]].to_numpy(), 
+                                                    df_pred[[label]].to_numpy())
+                else:
+                    res = metrics.mean_absolute_error(df_labels[[label]].to_numpy(), 
+                                                    df_pred[[label]].to_numpy())
+                    m = 'undefined metric --> defaults to MAE'
+
+
+                print(' ', label, m + ':', "   \t", round(res, 5))
+    
+    return df_pred    
---
+++ model.py
@@ -0,0 +1,97 @@
+"""
+Includes classes and functions used to create 
+a causal functional model for a given graph strucutre
+"""
+
+import numpy as np 
+import pandas as pd 
+from torch.utils.data import Dataset, DataLoader
+import torch as torch
+from torch import nn
+import copy
+
+
+class CFMDataset(Dataset):
+    """
+    Implementation of pytorch dataset class
+    to conveniently apply transforms, batchsizes, etc.
+    """
+    def __init__(self, features, labels, transform=None):
+
+        self.all_features = features
+        self.all_labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.all_labels)
+
+    def __getitem__(self, idx):
+        features = torch.tensor(self.all_features[idx], dtype=torch.double)
+        label = torch.tensor(self.all_labels[idx], dtype=torch.double)
+        
+        if self.transform:
+            features = self.transform(features)
+
+        return features, label
+
+def get_dataloader(df, feature_cols=['x11', 'x12', 'x13'],
+                    label_cols=['d31', 'l21'], batch_size=1):
+    """ 
+    Returns pytorch dataloader from given dataframe
+        
+    TBD: add support for incomplete datasets and 
+    drop rows with missing values in specified 
+    feature and label columns 
+    """
+    dataset = CFMDataset(features=df[feature_cols].to_numpy(), 
+                        labels=df[label_cols].to_numpy())
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, sampler=None,
+           batch_sampler=None, num_workers=0, collate_fn=None,
+           pin_memory=False, drop_last=False, timeout=0,
+           worker_init_fn=None)
+    return dataloader
+
+def func_block(dim_in, depth, activation=None):
+    """
+    Returns a MLP as specified by args
+    """
+    block = torch.nn.Sequential()
+    for i in range(int(depth)-1):
+        block.add_module('fc_' + str(i), nn.Linear(dim_in, dim_in))
+        if activation is not None:
+            block.add_module('activation_' + str(i), activation)
+    block.add_module('fc_' + str(i+1), nn.Linear(dim_in, 1))  
+    return block.double().apply(xavier_init)
+
+def xavier_init(m):  
+    """
+    Applies Xavier/Glorot initialization
+    """
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight.data)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias.data)
+
+def model_graph(graph, data, val_split, batch_size, depth, activation=None, optimizer='Adam'):
+    """
+    Extends the graph by adding models, dataloaders, 
+    and optimizers to nodes with inputs and returns it.
+
+    TBD: Add support for substituting models through 
+    user defined functions (in case of missing data).
+    Add more optimizers.
+    """
+    modeled_graph = copy.deepcopy(graph)
+    for node in modeled_graph:
+        if node['inputs'] is not None:
+            node['trainloader'] = get_dataloader(data[:val_split], feature_cols=node['inputs'],
+                                    label_cols=[node['name']], batch_size=batch_size)
+            node['valloader'] = get_dataloader(data[val_split:], feature_cols=node['inputs'],
+                                    label_cols=[node['name']], batch_size=batch_size)                        
+            node['model'] = func_block(len(node['inputs']), depth, activation)
+            if optimizer == 'Adam':
+                node['optimizer'] = torch.optim.Adam(node['model'].parameters())
+            else: 
+                print('Error: Choosen optimizer not implemented -> Default Adam is used')
+                node['optimizer'] = torch.optim.Adam(node['model'].parameters())
+    return modeled_graph
